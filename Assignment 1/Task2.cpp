3395dacc9a91a7ac92121f5a1e3f198e8f581d9a
Salwaa1209 <sdarmanastri@gmail.com>
2024-09-02 13:22:25 +0700

    Initial commit

---
+++ Assignment 1/Task2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double num1 = 5, num2 = 10, result = 0;
+    result = num1 * num2;;
+    cout << "The product of two numbers = " << result;
+
+    return 0;
+}
---
+++ Assignment 1/tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int addTwoNumbers(int a, int b){
+    int result;
+    result = a + b;
+    return resut;
+}
+
+int main(){
+    int num 1, num 2, sum;
+    std::cout << "Enter two numbers";
+    std::cin >> num 1 >> num 2;
+    sum = addTwoNumbers(num 1, num 2);
+    std::cout << "the sum is" << addTwoNumbers(a, b) << std::endl;
+
+    return 0; 
+}
